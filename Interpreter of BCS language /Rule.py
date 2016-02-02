2831007c69de8e1fa7c3b494cff9ca4cbff43148
mathooo <trojak@mail.muni.cz>
2016-02-02 14:27:04 +0100

    Added replace, formComplex, degrade, translate, removed reversibility + added and edited tests

--- Interpreter of BCS language /Rule.py
+++ Interpreter of BCS language /Rule.py
@@ -5,33 +5,22 @@ from Complex_Agent import *
 direction = " => "
 
 class Rule:
-    def __init__(self, left_hand_side, right_hand_side, reversible = False):
+    def __init__(self, left_hand_side, right_hand_side):
         self.left_hand_side = collections.Counter(left_hand_side)
         self.right_hand_side = collections.Counter(right_hand_side)
-        self.reversible = reversible
 
     def __eq__(self, other):
-        if not (self.reversible and other.reversible):
-            return False
-        else:
-            if self.reversible:
-                return (self.left_hand_side == other.left_hand_side and self.right_hand_side == other.right_hand_side) \
-                       or (self.right_hand_side == other.left_hand_side and self.left_hand_side == other.right_hand_side)
-            else:
-                return (self.left_hand_side == other.left_hand_side and self.right_hand_side == other.right_hand_side)
+        return (self.left_hand_side == other.left_hand_side and self.right_hand_side == other.right_hand_side)
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        global direction
-        if self.reversible:
-            direction = " <=> "
         return " + ".join(map(lambda k: k.__str__(), sorted(list(self.left_hand_side.elements())))) + direction \
                +  " + ".join(map(lambda k: k.__str__(), sorted(list(self.right_hand_side.elements()))))
 
     def __hash__(self):
-        return hash((str(self.left_hand_side), str(self.right_hand_side), str(self.reversible)))
+        return hash((str(self.left_hand_side), str(self.right_hand_side)))
 
     def __lt__(self, other):
         return self.__repr__() < other.__repr__()
@@ -42,12 +31,47 @@ class Rule:
     def getRightHandSide(self):
         return self.right_hand_side
 
-    def getReversible(self):
-        return  self.reversible
-
     def match(self, solution):
-        if self.reversible:
-            return compareCounters(copy.deepcopy(solution), self.getLeftHandSide()) or compareCounters(copy.deepcopy(solution), self.getRightHandSide())
-        else:
-            return compareCounters(copy.deepcopy(solution), self.getLeftHandSide())
+        return compareCounters(copy.deepcopy(solution), self.getLeftHandSide())
 
+    def replace(self, solution):
+        if not self.match(solution):
+            return solution
+        else:
+            left_size = len(list(self.getLeftHandSide().elements()))
+            right_size = len(list(self.getRightHandSide().elements()))
+            if left_size == right_size:
+                return self.changeStates(solution)
+            elif left_size > right_size:
+                if right_size == 0:
+                    return self.degrade(solution)
+                else:
+                    return self.formComplex(solution)
+            else:
+                if left_size == 0:
+                    return self.translate(solution)
+                else:
+                    return self.dissociateComplex(solution)
+
+
+    def changeStates(self, solution):
+        return #fuf
+
+    def degrade(self, solution):
+        return collections.Counter([])
+
+    def translate(self, solution):
+        return self.getRightHandSide()
+
+    def formComplex(self, solution):
+        new_solution = collections.Counter([])
+        for agent in solution.elements():
+            compartment = agent.getCompartment()
+            if isinstance(agent, Complex_Agent):
+                new_solution += agent.getFullComposition()
+            else:
+                new_solution += collections.Counter([agent])
+        return collections.Counter([Complex_Agent(list(new_solution.elements()), compartment)])
+
+    def dissociateComplex(self, solution):
+        return #uff
