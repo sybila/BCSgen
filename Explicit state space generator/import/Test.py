ef5ed22919d9aec995998c7f0f510757b091a045
mathooo <trojak@mail.muni.cz>
2016-02-24 10:59:35 +0100

    Working on rules importing

---
+++ Explicit state space generator/import/Test.py
@@ -0,0 +1,22 @@
+import unittest
+from import_rules import *
+
+class TestImport(unittest.TestCase):
+    def setUp(self):
+        self.agents = ["KaiC3A2::cyt", "KaiC3.KaiB::cyt", "KaiC3.KaiC.KaiB::cyt", "KaiC(S{p})::KaiC3.KaiC::KaiC3A2.KaiC::cyt"]
+        self.substate1 = ("KaiC3", "KaiC.KaiC.KaiC")
+        self.substate2 = ("KaiA2", "KaiA.KaiA")
+        self.substate3 = ("KaiC3A2", "KaiC3.KaiA2")
+        self.substitutions = [self.substate1, self.substate2, self.substate3]
+
+    def test_split_rule_agent(self):
+        self.assertEqual(split_rule_agent(self.agents[3]), [["KaiC(S{p})"], ["KaiC3", "KaiC"], ["KaiC3A2", "KaiC"], ["cyt"]])
+
+    def test_substitute(self):
+        self.assertEqual(substitute(self.substitutions, self.agents[0]), split_rule_agent("KaiC.KaiC.KaiC.KaiA.KaiA::cyt"))
+        self.assertEqual(substitute(self.substitutions, self.agents[1]), split_rule_agent('KaiC.KaiC.KaiC.KaiB::cyt'))
+        self.assertEqual(substitute(self.substitutions, self.agents[2]), split_rule_agent('KaiC.KaiC.KaiC.KaiC.KaiB::cyt'))
+        self.assertEqual(substitute(self.substitutions, self.agents[3]), split_rule_agent('KaiC(S{p})::KaiC.KaiC.KaiC.KaiC::KaiC.KaiC.KaiC.KaiA.KaiA.KaiC::cyt'))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestImport)
+unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ Explicit state space generator/import/import_rules.py
@@ -0,0 +1,67 @@
+import sys
+import copy
+
+"""
+Replaces all occurrences from defined substitutions for an agent
+:param substitutions: substitution list of pair (from, to)
+:param agents: list of agents (in sense of full composition)
+:return: list of replaced + not replaced agents
+"""
+def replace_agents(substitutions, agents):
+    new_agents = []
+    copy_agents = copy.deepcopy(agents)
+    for substate in substitutions:
+        for agent in agents:
+            if substate[0] == agent:
+                new_agents += substate[1].split(".")
+                copy_agents.remove(agent)
+                break
+    new_agents += copy_agents
+    return new_agents
+
+"""
+Splits rule to list of list by :: and . operators
+:param rule_agent: given string
+:return: list of agents
+"""
+def split_rule_agent(rule_agent):
+    splitted_agent = []
+    for agent in rule_agent.split("::"):
+        splitted_agent.append(agent.split("."))
+    return splitted_agent
+
+"""
+Applies substitutions on each agent in a rule
+:param substitutions: substitution list of pair (from, to)
+:param rule_agent: given string
+:return: list of replaced + not replaced agents
+"""
+def substitute(substitutions, rule_agent):
+    splitted_rule_agent = split_rule_agent(rule_agent)
+    new_rule_agent = []
+    while splitted_rule_agent != new_rule_agent:
+        new_rule_agent = splitted_rule_agent
+        splitted_rule_agent = map(lambda agents: replace_agents(substitutions, agents), splitted_rule_agent)
+    return new_rule_agent
+
+#first choose equal ones, then compatible ones !!!
+def flattenRule(rule):
+
+    return flattened_rule
+
+def flattenRuleSide(rule_side):
+    return
+'''
+agents_file = sys.argv[-1]
+rules_file = sys.argv[-2]
+
+substitutions = []
+
+with open(agents_file) as complexes:
+    for line in complexes:
+        substitutions.append(line.split("=="))
+
+with open(rules_file) as rules:
+    for line in rules:
+        line = line
+'''
