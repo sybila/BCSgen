f70689b4120a6a9e237e5d31bed2f5924432a6a2
mathooo <trojak@mail.muni.cz>
2016-10-20 20:51:09 +0200

    gui done, just connect to BCSgen

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,11 +2,9 @@ from Tkinter import *
 from tkFileDialog import askopenfilename
 
 class Application(Frame):
-	def ready(self):
+	def get_ready(self, *args):
 		if self.rules and self.initial and self.vertices and self.edges:
-			return True
-		else:
-			return False
+			self.compute.config(state=NORMAL)
 
 	def compute(self):
 		return
@@ -17,7 +15,6 @@ class Application(Frame):
 		self.text_rules.delete(0, END)
 		self.text_rules.insert(END, self.rules.__str__())
 		self.text_rules.config(state="readonly")
-		print self.rules
 
 	def set_initial(self):
 		self.initial = askopenfilename()
@@ -25,7 +22,6 @@ class Application(Frame):
 		self.text_initial.delete(0, END)
 		self.text_initial.insert(END, self.initial.__str__())
 		self.text_initial.config(state="readonly")
-		print self.initial
 
 	def set_vertices(self):
 		self.vertices = askopenfilename()
@@ -33,7 +29,6 @@ class Application(Frame):
 		self.text_vert.delete(0, END)
 		self.text_vert.insert(END, self.vertices.__str__())
 		self.text_vert.config(state="readonly")
-		print self.vertices
 
 	def set_edges(self):
 		self.edges = askopenfilename()
@@ -41,15 +36,12 @@ class Application(Frame):
 		self.text_edg.delete(0, END)
 		self.text_edg.insert(END, self.edges.__str__())
 		self.text_edg.config(state="readonly")
-		print self.edges
 
 	def set_parallel(self):
 		self.parallel = not self.parallel
-		print self.parallel
 
 	def set_memo(self):
 		self.memoization = not self.memoization
-		print self.memoization
 
 	def print_all(self):
 		print "rules: ", self.rules
@@ -64,13 +56,13 @@ class Application(Frame):
 		self.mes = Message(root,text='Input', width=300, font="bold", borderwidth=8, relief= RIDGE)
 		self.mes.grid(row=0, column=0, columnspan=2, ipadx=205)
 
-		self.text_rules = Entry(root,width=30, state="readonly", readonlybackground='white')
+		self.text_rules = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.rulesVar)
 		self.text_rules.grid(row=1, column=1)
 
 		self.button_rules = Button(root,text="Rules",command=self.set_rules, width=25)
 		self.button_rules.grid(row=1, column=0)
 
-		self.text_initial = Entry(root,width=30, state="readonly", readonlybackground='white')
+		self.text_initial = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.initVar)
 		self.text_initial.grid(row=2, column=1)
 
 		self.button_init = Button(root,text="Initial state",command=self.set_initial,  width=25)
@@ -92,13 +84,13 @@ class Application(Frame):
 		self.mes = Message(root,text='Output', width=300, font="bold", borderwidth=8, relief= RIDGE)
 		self.mes.grid(row=6, column=0, columnspan=2, ipadx=200)
 
-		self.text_vert = Entry(root,width=30, state="readonly", readonlybackground='white')
+		self.text_vert = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.vertVar)
 		self.text_vert.grid(row=7, column=1)
 
 		self.button_vert = Button(root,text="Vertices",command=self.set_vertices, width=25)
 		self.button_vert.grid(row=7, column=0)
 
-		self.text_edg = Entry(root,width=30, state="readonly", readonlybackground='white')
+		self.text_edg = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.edgVar)
 		self.text_edg.grid(row=8, column=1)
 
 		self.button_edg = Button(root,text="Edges",command=self.set_edges, width=25)
@@ -107,9 +99,10 @@ class Application(Frame):
 		self.compute = Button(root,text="Compute",command=self.compute, width=25, state=DISABLED)
 		self.compute.grid(row=9, column=1)
 
-		if self.ready():
-			print 'yes'
-			self.compute.config(state=NORMAL)
+		self.vertVar.trace("w", self.get_ready)
+		self.edgVar.trace("w", self.get_ready)
+		self.initVar.trace("w", self.get_ready)
+		self.rulesVar.trace("w", self.get_ready)
 
 
 	def __init__(self, master=None):
@@ -120,6 +113,10 @@ class Application(Frame):
 		self.edges = None
 		self.parallel = False
 		self.memoization = False
+		self.vertVar = StringVar()
+		self.edgVar = StringVar()
+		self.initVar = StringVar()
+		self.rulesVar = StringVar()
 		self.bound = StringVar()
 		self.grid()
 		self.createWidgets()
