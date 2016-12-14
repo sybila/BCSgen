1eba39d886c352c5f39feee74c3087e50c03f096
mathooo <trojak@mail.muni.cz>
2016-12-14 17:02:20 +0100

    Fixed gui, working on #16

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,9 +1,9 @@
 from time import time
 import sys
 import os.path
-sys.path.append(os.path.abspath('../'))
-import Import as Import
+sys.path.append(os.path.abspath('../Core/'))
 import State_space_generator as Gen
+import Implicit_reaction_network_generator as Implicit
 
 from Tkinter import *
 from tkFileDialog import askopenfilename
@@ -16,7 +16,7 @@ class Application(Frame):
     Checks if all fields are filled
     """
     def get_ready(self, *args):
-        if self.model and self.vertices and self.edges and self.bound.get() != "":
+        if self.model and self.vertices and self.edges and self.reactions and self.bound.get() != "":
            self.compute.config(state=NORMAL)
 
     """
@@ -26,10 +26,10 @@ class Application(Frame):
         self.compute.config(state=DISABLED)
         self.compute.config(text="Computing...")
 
-        rules, state = Import.import_model(self.model)
-        states = {state}
-        bound = int(self.bound.get())
-        done = Gen.work_manager(states, rules, self.vertices, self.edges, bound, self.parallel, self.memoization)
+        myNet, state = Implicit.generateReactions(self.model)
+        states, edges, orderedAgents = Gen.generateStateSpace(myNet, state, int(self.bound.get()))
+        Gen.printStateSpace(states, edges, orderedAgents, self.vertices, self.edges)
+        myNet.printReactions(self.reactions)
 
         self.compute.config(state=NORMAL)
         self.compute.config(text="Finish")
@@ -65,81 +65,74 @@ class Application(Frame):
         self.text_edg.insert(END, self.edges.__str__())
         self.text_edg.config(state="readonly")
 
-    """
-    Sets variable parallel in order to (not) apply paralle computing
-    """
-    def set_parallel(self):
-        self.parallel = not self.parallel
+    def set_reactions(self):
+        self.reactions = askopenfilename()
+        self.text_reaction.config(state=NORMAL)
+        self.text_reaction.delete(0, END)
+        self.text_reaction.insert(END, self.reactions.__str__())
+        self.text_reaction.config(state="readonly")
 
-    """
-    Sets variable memoization in order to (not) apply memoization
-    """
-    def set_memo(self):
-        self.memoization = not self.memoization
 
     """
     This is where visual behaviour is maintained
     """
     def createWidgets(self):
-        self.mes = Message(root,text='Input', width=300, font="bold", borderwidth=8, relief= RIDGE)
-        self.mes.grid(row=0, column=0, columnspan=2, ipadx=205)
+        self.mes = Message(root,text='Input', width=200, font="bold", borderwidth=8, relief= RIDGE)
+        self.mes.grid(row=0, column=0, columnspan=2, ipadx=105)
 
-        self.text_model = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.modelVar)
+        self.text_model = Entry(root,width=20, state="readonly", readonlybackground='white', textvariable=self.modelVar)
         self.text_model.grid(row=1, column=1)
 
-        self.button_rules = Button(root,text="Model",command=self.set_model, width=25)
+        self.button_rules = Button(root,text="Model",command=self.set_model, width=15)
         self.button_rules.grid(row=1, column=0)
 
-        self.bound_name = Label(root,text="Bound:",  width=25).grid(row=3, column=0)
-        self.bound_wid = Entry(root,textvariable=self.bound, width=30)
-        self.bound_wid.grid(row=3, column=1)
+        self.bound_name = Label(root,text="Bound:",  width=20).grid(row=2, column=0)
+        self.bound_wid = Entry(root,textvariable=self.bound, width=20)
+        self.bound_wid.grid(row=2, column=1)
 
-        self.mes = Message(root,text='Choose techniques to be applied', width=300, font="bold", borderwidth=8, relief= RIDGE)
-        self.mes.grid(row=4, column=0, columnspan=2, ipadx=95)
+        self.mes = Message(root,text='Output', width=200, font="bold", borderwidth=8, relief= RIDGE)
+        self.mes.grid(row=3, column=0, columnspan=2, ipadx=100)
 
-        self.c1 = Checkbutton(root,text="Parallel", command=self.set_parallel)
-        self.c1.grid(row=5, column=0)
+        self.text_vert = Entry(root,width=20, state="readonly", readonlybackground='white', textvariable=self.vertVar)
+        self.text_vert.grid(row=4, column=1)
 
-        self.c2 = Checkbutton(root,text="Memoization", command=self.set_memo) 
-        self.c2.grid(row=5, column=1)
+        self.button_vert = Button(root,text="Vertices",command=self.set_vertices, width=15)
+        self.button_vert.grid(row=4, column=0)
 
-        self.mes = Message(root,text='Output', width=300, font="bold", borderwidth=8, relief= RIDGE)
-        self.mes.grid(row=6, column=0, columnspan=2, ipadx=200)
+        self.text_edg = Entry(root,width=20, state="readonly", readonlybackground='white', textvariable=self.edgVar)
+        self.text_edg.grid(row=5, column=1)
 
-        self.text_vert = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.vertVar)
-        self.text_vert.grid(row=7, column=1)
+        self.button_edg = Button(root,text="Edges",command=self.set_edges, width=15)
+        self.button_edg.grid(row=5, column=0)
 
-        self.button_vert = Button(root,text="Vertices",command=self.set_vertices, width=25)
-        self.button_vert.grid(row=7, column=0)
+        self.text_reaction = Entry(root,width=20, state="readonly", readonlybackground='white', textvariable=self.reactVar)
+        self.text_reaction.grid(row=6, column=1)
 
-        self.text_edg = Entry(root,width=30, state="readonly", readonlybackground='white', textvariable=self.edgVar)
-        self.text_edg.grid(row=8, column=1)
+        self.button_reaction = Button(root,text="Reactions",command=self.set_reactions, width=15)
+        self.button_reaction.grid(row=6, column=0)
 
-        self.button_edg = Button(root,text="Edges",command=self.set_edges, width=25)
-        self.button_edg.grid(row=8, column=0)
+        self.compute = Button(root,text="Compute",command=self.compute, width=15, state=DISABLED)
+        self.compute.grid(row=7, column=1)
 
-        self.compute = Button(root,text="Compute",command=self.compute, width=25, state=DISABLED)
-        self.compute.grid(row=9, column=1)
-
-        self.exit = Button(root,text="Cancel",command=root.destroy, width=25)
-        self.exit.grid(row=9, column=0)
+        self.exit = Button(root,text="Cancel",command=root.destroy, width=15)
+        self.exit.grid(row=7, column=0)
 
         self.vertVar.trace("w", self.get_ready)
         self.edgVar.trace("w", self.get_ready)
         self.modelVar.trace("w", self.get_ready)
         self.bound.trace("w", self.get_ready)
+        self.reactVar.trace("w", self.get_ready)
 
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.rules = None
-        self.initial = None
+        self.reactions = None
         self.vertices = None
         self.edges = None
-        self.parallel = False
-        self.memoization = False
+        self.model = None
         self.vertVar = StringVar()
         self.edgVar = StringVar()
+        self.reactVar = StringVar()
         self.modelVar = StringVar()
         self.bound = StringVar(value="")
         self.grid()
