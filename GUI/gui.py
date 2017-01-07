ee604dc726b2375207906fafbd8810bf6f03f6ad
mathooo <trojak@mail.muni.cz>
2017-01-07 16:05:13 +0100

    GUI done, close #42 #39 #38

--- GUI/gui.py
+++ GUI/gui.py
@@ -34,17 +34,14 @@ def createButton(it, text, to_connect, movex, movey, disabled):
     button.setDisabled(disabled)
     return button
 
-class Worker(QtCore.QObject):
+class ReactionWorker(QtCore.QObject):
     taskFinished = QtCore.pyqtSignal()
     def __init__(self, parent=None):
         QtCore.QObject.__init__(self, parent)
 
-        self.reactionsFile = None
-        self.stateSpaceFile = None
         self.modelFile = None
+        self.reactionsFile = None
         self.logFile = None
-        self.lenStates = None
-        self.lenEdges = None
         self.lenReactions = None
 
         self.TheWorker = QtCore.QThread()
@@ -54,17 +51,14 @@ class Worker(QtCore.QObject):
     def getTheWorker(self):
         return self.TheWorker
 
-    def setLenReactions(self, lenReactions):
-        self.lenReactions = lenReactions
-
-    def getLenReactions(self):
-        return self.lenReactions
-
     def setModelFile(self, modelFile):
         self.modelFile = modelFile
 
     def getModelFile(self):
         return self.modelFile
+
+    def setLenReactions(self, lenReactions):
+        self.lenReactions = lenReactions
 
     def setReactionsFile(self, reactionsFile):
         self.reactionsFile = reactionsFile
@@ -72,14 +66,51 @@ class Worker(QtCore.QObject):
     def getReacionsFile(self):
         return self.reactionsFile
 
+    def setLogFile(self, logFile):
+        self.logFile = logFile
+
+    def compute_reactions(self):
+        myNet, state, networkStatus, message = Implicit.initializeNetwork(self.modelFile)
+        myNet = Implicit.generateReactions(myNet)
+        myNet.printReactions(self.reactionsFile)
+        self.lenReactions.setText('Reactions: ' + str(myNet.getNumOfReactions()))
+        if self.logFile and not networkStatus:
+            self.save_log(message)
+        self.taskFinished.emit() 
+
+    def save_log(self, message):
+        f = open(self.logFile,'w')
+        f.write(message[:-30])
+        f.close()
+
+class StateSpaceWorker(QtCore.QObject):
+    taskFinished = QtCore.pyqtSignal()
+    def __init__(self, parent=None):
+        QtCore.QObject.__init__(self, parent)
+        
+        self.modelFile = None
+        self.stateSpaceFile = None
+        self.lenStates = None
+        self.lenEdges = None
+        
+        self.TheWorker = QtCore.QThread()
+        self.moveToThread(self.TheWorker)
+        self.TheWorker.start()
+
+    def getTheWorker(self):
+        return self.TheWorker
+
+    def setModelFile(self, modelFile):
+        self.modelFile = modelFile
+
+    def getModelFile(self):
+        return self.modelFile
+
     def setStateSpaceFile(self, stateSpaceFile):
         self.stateSpaceFile = stateSpaceFile
 
     def getStateSpaceFile(self):
         return self.stateSpaceFile
-
-    def setLogFile(self, logFile):
-        self.logFile = logFile
 
     def setLenStates(self, lenObj):
         self.lenStates = lenObj
@@ -97,28 +128,16 @@ class Worker(QtCore.QObject):
         self.lenEdges.setText('Edges: ' + str(len(edges)))
         self.taskFinished.emit() 
 
-    def compute_reactions(self):
-        myNet, state, networkStatus, message = Implicit.initializeNetwork(self.modelFile)
-        myNet = Implicit.generateReactions(myNet)
-        myNet.printReactions(self.reactionsFile)
-        self.lenReactions.setText('Reactions: ' + str(myNet.getNumOfReactions()))
-        if self.logFile and not networkStatus:
-            self.save_log(message)
-
-    def save_log(self, message):
-        f = open(self.logFile,'w')
-        f.write(message[:-30])
-        f.close()
-
 class MainWindow(QtGui.QWidget):
     def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
 
         #########################################
 
         # setup
 
-        QtGui.QWidget.__init__(self, parent)
-        self.worker = Worker()
+        self.stateWorker = StateSpaceWorker()
+        self.reactionWorker = ReactionWorker()
 
         #########################################
 
@@ -146,14 +165,14 @@ class MainWindow(QtGui.QWidget):
         # progres bar
 
         self.progress_bar_states = createProgressBar(self, 10, 130)
-        self.worker.taskFinished.connect(self.progressbarStatesOnFinished)
+        self.stateWorker.taskFinished.connect(self.progressbarStatesOnFinished)
 
         # Compute states button
 
-        self.compute_space_button = createButton(self, 'Compute', self.worker.compute_space, 160, 170, True)
+        self.compute_space_button = createButton(self, 'Compute', self.stateWorker.compute_space, 160, 170, True)
         self.compute_space_button.clicked.connect(self.progressbarStatesOnStart)
 
-        self.cancel_state = createButton(self, 'Cancel', self.cancel_computation, 10, 170, True)
+        self.cancel_state = createButton(self, 'Cancel', self.cancel_computation_states, 10, 170, True)
         self.cancel_state.clicked.connect(self.progressbarStatesOnFinished)
 
         #########################################
@@ -175,14 +194,14 @@ class MainWindow(QtGui.QWidget):
         # progres bar
 
         self.progress_bar_reactions = createProgressBar(self, 10, 290)
-        self.worker.taskFinished.connect(self.progressbarReactionsOnFinished)
+        self.reactionWorker.taskFinished.connect(self.progressbarReactionsOnFinished)
 
         # Compute reactions button
 
-        self.compute_reactions_button = createButton(self, 'Compute', self.worker.compute_reactions, 160, 330, True)
+        self.compute_reactions_button = createButton(self, 'Compute', self.reactionWorker.compute_reactions, 160, 330, True)
         self.compute_reactions_button.clicked.connect(self.progressbarReactionsOnStart)
 
-        self.cancel_rxns = createButton(self, 'Cancel', self.cancel_computation, 10, 330, True)
+        self.cancel_rxns = createButton(self, 'Cancel', self.cancel_computation_reactions, 10, 330, True)
         self.cancel_rxns.clicked.connect(self.progressbarReactionsOnFinished)
 
         #########################################
@@ -200,12 +219,24 @@ class MainWindow(QtGui.QWidget):
         self.progress_bar_states.setRange(0,1)
         self.progress_bar_states.setValue(1)
 
+        self.compute_space_button.setDisabled(True)
+        self.compute_space_button.setText('Finished')
+        self.cancel_state.setDisabled(True)
+        self.stateSpace.setDisabled(True)
+        self.model.setDisabled(True)
+
     def progressbarReactionsOnStart(self): 
         self.progress_bar_reactions.setRange(0,0)
 
     def progressbarReactionsOnFinished(self):
         self.progress_bar_reactions.setRange(0,1)
         self.progress_bar_reactions.setValue(1)
+
+        self.compute_reactions_button.setDisabled(True)
+        self.compute_reactions_button.setText('Finished')
+        self.cancel_rxns.setDisabled(True)
+        self.reactions.setDisabled(True)
+        self.model.setDisabled(True)
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
@@ -227,40 +258,45 @@ class MainWindow(QtGui.QWidget):
         qp.drawLine(10, 365, 310, 365)
 
     def open_model(self):
-        self.worker.setModelFile(QFileDialog.getOpenFileName(self, 'Choose model', directory = '../Examples/inputs/', filter =".bcs (*.bcs)"))
-        self.model_text.setText(self.worker.getModelFile())
-        if self.worker.getStateSpaceFile():
+        self.stateWorker.setModelFile(QFileDialog.getOpenFileName(self, 'Choose model', directory = '../Examples/inputs/', filter =".bcs (*.bcs)"))
+        self.model_text.setText(self.stateWorker.getModelFile())
+        self.reactionWorker.setModelFile(self.stateWorker.getModelFile())
+        if self.stateWorker.getStateSpaceFile():
             self.compute_space_button.setDisabled(False)
             self.cancel_state.setDisabled(False)
-        if self.worker.getReacionsFile():
+        if self.reactionWorker.getReacionsFile():
             self.compute_reactions_button.setDisabled(False)
             self.cancel_rxns.setDisabled(False)
             self.log.setDisabled(False)
 
     def save_stateSpace(self):
-        self.worker.setStateSpaceFile(QFileDialog.getSaveFileName(self, 'Choose output file', filter =".json (*.json)"))
-        self.stateSpace_text.setText(self.worker.getStateSpaceFile())
-        self.worker.setLenStates(self.num_of_states)
-        self.worker.setLenEdges(self.num_of_edges)
-        if self.worker.getModelFile():
+        self.stateWorker.setStateSpaceFile(QFileDialog.getSaveFileName(self, 'Choose output file', filter =".json (*.json)"))
+        self.stateSpace_text.setText(self.stateWorker.getStateSpaceFile())
+        self.stateWorker.setLenStates(self.num_of_states)
+        self.stateWorker.setLenEdges(self.num_of_edges)
+        if self.stateWorker.getModelFile():
             self.compute_space_button.setDisabled(False)
             self.cancel_state.setDisabled(False)
 
     def save_reactions(self):
-        self.worker.setReactionsFile(QFileDialog.getSaveFileName(self, 'Choose output file', filter =".txt (*.txt)"))
-        self.reactions_text.setText(self.worker.getReacionsFile())
-        self.worker.setLenReactions(self.num_of_reactions)
-        if self.worker.getModelFile():
+        self.reactionWorker.setReactionsFile(QFileDialog.getSaveFileName(self, 'Choose output file', filter =".txt (*.txt)"))
+        self.reactions_text.setText(self.reactionWorker.getReacionsFile())
+        self.reactionWorker.setLenReactions(self.num_of_reactions)
+        if self.reactionWorker.getModelFile():
             self.compute_reactions_button.setDisabled(False)
             self.cancel_rxns.setDisabled(False)
             self.log.setDisabled(False)
 
     def save_log(self):
-        self.worker.setLogFile(QFileDialog.getSaveFileName(self, 'Choose log file', filter =".log (*.log)"))
+        self.reactionWorker.setLogFile(QFileDialog.getSaveFileName(self, 'Choose log file', filter =".log (*.log)"))
 
-    def cancel_computation(self):
-        if not self.worker.getTheWorker().wait(1000):
-            self.worker.getTheWorker().terminate()
+    def cancel_computation_states(self):
+        if not self.stateWorker.getTheWorker().wait(1000):
+            self.stateWorker.getTheWorker().terminate()
+
+    def cancel_computation_reactions(self):
+        if not self.reactionWorker.getTheWorker().wait(1000):
+            self.reactionWorker.getTheWorker().terminate()
 
 app = QtGui.QApplication(sys.argv)
 main = MainWindow()
