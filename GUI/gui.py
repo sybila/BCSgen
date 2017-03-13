9108182a6ca5ca6d3d6e7725d00c0f3f19b186fb
mathooo <trojak@mail.muni.cz>
2017-03-13 19:48:11 +0100

    Adding textedit area

--- GUI/gui.py
+++ GUI/gui.py
@@ -134,11 +134,28 @@ class StateSpaceWorker(QtCore.QObject):
         self.lenEdges.setText('Edges: ' + str(len(edges)))
         self.taskFinished.emit() 
 
-class MainWindow(QtGui.QWidget):
+def createAction(it, title, shortcut, tip, connectWith):
+    action = QtGui.QAction(title, it)
+    action.setShortcut(shortcut)
+    action.setStatusTip(tip)
+    action.triggered.connect(connectWith)
+    return action
+
+class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtGui.QMainWindow.__init__(self, parent)
 
         #########################################
+
+        self.load = createAction(self, "&Load", "Ctrl+L", 'Load model from a file.', self.open_model)
+        self.exit = createAction(self, "&Exit", "Ctrl+E", 'Exit program.', self.close)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(self.load)
+        fileMenu.addAction(self.exit)
 
         # setup
 
@@ -157,47 +174,59 @@ class MainWindow(QtGui.QWidget):
         self.stateSpaceEstimate = 0
         self.reactionsEstimate = 0
 
+
+        #########################################
+
+        # text area
+
+        self.textBox = QTextEdit(self)
+        self.textBox.resize(590, 400)
+        self.textBox.move(10, 20)
+        #self.textBox.cursorPositionChanged.connect(self.textEdited)
+        self.textBox.setLineWrapColumnOrWidth(590)
+        self.textBox.setLineWrapMode(QtGui.QTextEdit.FixedColumnWidth)
+
         #########################################
 
         # model file button
 
-        self.model = createButton(self, 'Model file', self.open_model, 10, 10, False)
+        self.model = createButton(self, 'Model file', self.open_model, 610, 10, False)
 
-        self.model_text = createTextBox(self, None, None, 160, 10, True)
+        self.model_text = createTextBox(self, None, None, 760, 10, True)
 
         #########################################
 
         # state space file button
 
-        self.stateSpace = createButton(self, 'State space file', self.save_stateSpace, 10, 50, False)
+        self.stateSpace = createButton(self, 'State space file', self.save_stateSpace, 610, 50, False)
 
-        self.stateSpace_text = createTextBox(self, None, None, 160, 50, True)
+        self.stateSpace_text = createTextBox(self, None, None, 760, 50, True)
 
         # results fields
 
         style = '''QLineEdit {background-color: rgb(214, 214, 214); border: none ; }'''
-        self.num_of_states = createTextBox(self, 'States: ', style, 10, 90, True)
+        self.num_of_states = createTextBox(self, 'States: ', style, 610, 90, True)
 
-        self.num_of_edges = createTextBox(self, 'Edges: ', style, 160, 90, True)
+        self.num_of_edges = createTextBox(self, 'Edges: ', style, 760, 90, True)
 
         # progres bar
 
-        self.progress_bar_states = createProgressBar(self, 10, 130)
+        self.progress_bar_states = createProgressBar(self, 610, 130)
         self.stateWorker.taskFinished.connect(self.progressbarStatesOnFinished)
 
         # time bar
 
         self.runningStates = QtGui.QLabel(self)
         self.runningStates.setText(" Na:Na:Na /  Na:Na:Na")
-        self.runningStates.move(175, 135)
+        self.runningStates.move(775, 135)
 
         # Compute states button
 
-        self.compute_space_button = createButton(self, 'Compute', self.stateWorker.compute_space, 160, 170, True)
+        self.compute_space_button = createButton(self, 'Compute', self.stateWorker.compute_space, 760, 170, True)
         self.compute_space_button.clicked.connect(self.progressbarStatesOnStart)
         self.compute_space_button.clicked.connect(self.startStateSpaceTimer)
 
-        self.cancel_state = createButton(self, 'Cancel', self.cancel_computation_states, 10, 170, True)
+        self.cancel_state = createButton(self, 'Cancel', self.cancel_computation_states, 610, 170, True)
         self.cancel_state.clicked.connect(self.progressbarStatesOnFinished)
         self.cancel_state.clicked.connect(self.stateSpaceCanceled)
 
@@ -205,36 +234,36 @@ class MainWindow(QtGui.QWidget):
 
         # reactions file button
 
-        self.reactions = createButton(self, 'Reactions file', self.save_reactions, 10, 210, False)
+        self.reactions = createButton(self, 'Reactions file', self.save_reactions, 610, 210, False)
 
-        self.reactions_text = createTextBox(self, None, None, 160, 210, True)
+        self.reactions_text = createTextBox(self, None, None, 760, 210, True)
 
         # result field
 
-        self.num_of_reactions = createTextBox(self, 'Reactions: ', style, 10, 250, True)
+        self.num_of_reactions = createTextBox(self, 'Reactions: ', style, 610, 250, True)
 
         # log file
 
-        self.log = createButton(self, 'Save log', self.save_log, 160, 250, True)
+        self.log = createButton(self, 'Save log', self.save_log, 760, 250, True)
 
         # progres bar
 
-        self.progress_bar_reactions = createProgressBar(self, 10, 290)
+        self.progress_bar_reactions = createProgressBar(self, 610, 290)
         self.reactionWorker.taskFinished.connect(self.progressbarReactionsOnFinished)
 
         # time bar
 
         self.runningReactions = QtGui.QLabel(self)
         self.runningReactions.setText(" Na:Na:Na /  Na:Na:Na")
-        self.runningReactions.move(175, 305)
+        self.runningReactions.move(775, 305)
 
         # Compute reactions button
 
-        self.compute_reactions_button = createButton(self, 'Compute', self.reactionWorker.compute_reactions, 160, 330, True)
+        self.compute_reactions_button = createButton(self, 'Compute', self.reactionWorker.compute_reactions, 760, 330, True)
         self.compute_reactions_button.clicked.connect(self.progressbarReactionsOnStart)
         self.compute_reactions_button.clicked.connect(self.stateReactionsTimes)
 
-        self.cancel_rxns = createButton(self, 'Cancel', self.cancel_computation_reactions, 10, 330, True)
+        self.cancel_rxns = createButton(self, 'Cancel', self.cancel_computation_reactions, 610, 330, True)
         self.cancel_rxns.clicked.connect(self.progressbarReactionsOnFinished)
         self.cancel_rxns.clicked.connect(self.reactionsCanceled)
 
@@ -242,7 +271,7 @@ class MainWindow(QtGui.QWidget):
 
         # quit button
 
-        self.exit = createButton(self, 'Exit', QtCore.QCoreApplication.instance().quit, 10, 370, False)
+        self.exit = createButton(self, 'Exit', QtCore.QCoreApplication.instance().quit, 610, 370, False)
         self.exit.clicked.connect(self.cancel_computation_states)
         self.exit.clicked.connect(self.cancel_computation_reactions)
 
@@ -312,15 +341,15 @@ class MainWindow(QtGui.QWidget):
         pen = QtGui.QPen(QtCore.Qt.gray, 4, QtCore.Qt.SolidLine)
 
         qp.setPen(pen)
-        qp.drawLine(10, 45, 310, 45)
+        qp.drawLine(610, 45, 910, 45)
 
         qp.setPen(pen)
-        qp.drawLine(10, 205, 310, 205)
+        qp.drawLine(610, 205, 910, 205)
 
         qp.setPen(pen)
-        qp.drawLine(10, 365, 310, 365)
+        qp.drawLine(610, 365, 910, 365)
 
-        qp.drawPixmap(225,372,QPixmap("icons/logo.png"))
+        qp.drawPixmap(825,372,QPixmap("icons/logo.png"))
 
     def open_model(self):
         self.stateWorker.setModelFile(QFileDialog.getOpenFileName(self, 'Choose model', directory = '../Examples/inputs/', filter =".bcs (*.bcs)"))
@@ -384,7 +413,7 @@ app_icon.addFile('icons/128x128.png', QtCore.QSize(128,128))
 app.setWindowIcon(app_icon)
 
 main = MainWindow()
-main.setFixedSize(320, 410)
+main.setFixedSize(920, 510)
 main.setWindowTitle('BCSgen')
 main.show()
 sys.exit(app.exec_())
