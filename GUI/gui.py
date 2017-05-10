e36165bbdfbfed8d6ca1e97149039d7874aac2e5
mathooo <trojak@mail.muni.cz>
2017-05-10 10:08:47 +0200

    Changed plotting library from pyqtgraph to matplotlib

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,7 +5,7 @@ import markdown
 import numpy as np
 from PyQt4 import QtGui, QtCore, QtWebKit
 from PyQt4.QtGui import *
-import pyqtgraph as pg
+import matplotlib.pyplot as plt
 import signal
 import sympy
 import random 
@@ -52,32 +52,137 @@ def createAction(it, title, shortcut, tip, connectWith, icon):
 	action.triggered.connect(connectWith)
 	return action
 
-class SimulationPlot(pg.GraphicsWindow):
-	def __init__(self, data, times, translations, parent = None):
+class SimulationPlot(QWidget):
+	def __init__(self, data, times, translations, parent= None):
 		super(SimulationPlot, self).__init__()
 
-		self.resize(1000,600)
-		self.setWindowTitle('BCSgen - model simulation')
+		fig_size = plt.rcParams["figure.figsize"]
+		fig_size[0] = 9
+		fig_size[1] = 6
+		plt.rcParams["figure.figsize"] = fig_size
 
-		# Enable antialiasing for prettier plots
-		pg.setConfigOptions(antialias=True)
+		fig, self.ax = plt.subplots()
 
-		times = [int(time*(10**12)) for time in times]
-
-		myPlot = self.addPlot(title="Simulation results")
-		myPlot.addLegend()
-		myPlot.setLabel('bottom', 'Time')
-		myPlot.setLabel('left', 'Quantity')
+		fig.canvas.set_window_title('Simulation results')
 
 		size = len(data[0])
-		ratio = 100/size
-		for i in range(size):
-			myPlot.plot(x = times, y = self.column(data, i), pen=((i+1)*ratio,100), name = translations[i])
 
-		self.show()
+		for i in range(size):
+			self.ax.plot(times, self.column(data, i), label=translations[i])
+
+		self.ax.legend(loc='upper center', bbox_to_anchor=(0.5,-0.1), ncol=4)
+
+		numOfLines = round(len(translations)/4.0)
+		fig.subplots_adjust(bottom=0.15*numOfLines) # need to be changed to slower function
+		self.ax.grid('on')
+
+		self.interactive_legend().show()
+
+	def interactive_legend(self):
+		if self.ax is None:
+			self.ax = plt.gca()
+		if self.ax.legend_ is None:
+			self.ax.legend()
+
+		return InteractiveLegend(self.ax.legend_)
 
 	def column(self, matrix, i):
-		return [row[i] for row in matrix]
+ 		return [row[i] for row in matrix]
+
+class InteractiveLegend(object):
+	def __init__(self, legend):
+		self.legend = legend
+		self.fig = legend.axes.figure
+
+		self.lookup_artist, self.lookup_handle = self._build_lookups(legend)
+		self._setup_connections()
+
+		self.update()
+
+	def _setup_connections(self):
+		for artist in self.legend.texts + self.legend.legendHandles:
+			artist.set_picker(8) # 8 points tolerance
+
+		self.fig.canvas.mpl_connect('pick_event', self.on_pick)
+		self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+
+	def _build_lookups(self, legend):
+		labels = [t.get_text() for t in legend.texts]
+		handles = legend.legendHandles
+		label2handle = dict(zip(labels, handles))
+		handle2text = dict(zip(handles, legend.texts))
+
+		lookup_artist = {}
+		lookup_handle = {}
+		for artist in legend.axes.get_children():
+			if artist.get_label() in labels:
+				handle = label2handle[artist.get_label()]
+				lookup_handle[artist] = handle
+				lookup_artist[handle] = artist
+				lookup_artist[handle2text[handle]] = artist
+
+		lookup_handle.update(zip(handles, handles))
+		lookup_handle.update(zip(legend.texts, handles))
+
+		return lookup_artist, lookup_handle
+
+	def on_pick(self, event):
+		handle = event.artist
+		if handle in self.lookup_artist:
+			artist = self.lookup_artist[handle]
+			artist.set_visible(not artist.get_visible())
+			self.update()
+
+	def on_click(self, event):
+		if event.button == 3:
+			visible = False
+		elif event.button == 2:
+			visible = True
+		else:
+			return
+
+		for artist in self.lookup_artist.values():
+			artist.set_visible(visible)
+		self.update()
+
+	def update(self):
+		for artist in self.lookup_artist.values():
+			handle = self.lookup_handle[artist]
+			if artist.get_visible():
+				handle.set_visible(True)
+			else:
+				handle.set_visible(False)
+		self.fig.canvas.draw()
+
+	def show(self):
+		plt.show()
+
+# class SimulationPlot(pg.GraphicsWindow):
+# 	def __init__(self, data, times, translations, parent = None):
+# 		super(SimulationPlot, self).__init__()
+
+# 		self.resize(1000,600)
+# 		self.setWindowTitle('BCSgen - model simulation')
+
+# 		# Enable antialiasing for prettier plots
+# 		pg.setConfigOptions(antialias=True)
+
+# 		times = [int(time*(10**12)) for time in times]
+
+# 		myPlot = self.addPlot(title="Simulation results")
+# 		myPlot.addLegend()
+# 		myPlot.setLabel('bottom', 'Time')
+# 		myPlot.setLabel('left', 'Quantity')
+
+# 		size = len(data[0])
+# 		ratio = 100/size
+# 		for i in range(size):
+# 			myPlot.plot(x = times, y = self.column(data, i), pen=((i+1)*ratio,100), name = translations[i])
+
+# 		self.show()
+
+# 	def column(self, matrix, i):
+# 		return [row[i] for row in matrix]
 
 """
 class GraphVisual
