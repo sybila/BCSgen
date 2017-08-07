c7e0f906e58d3f50277ab94de97d2e53a6c13341
mathooo <trojak@mail.muni.cz>
2017-08-07 09:29:13 +0200

    Working on full syntax

--- Core/BCSL_objects/Semantic_Function.py
+++ Core/BCSL_objects/Semantic_Function.py
@@ -74,37 +74,73 @@ Creating Rule objects from parsed tree
 """
 
 def createRules(rules, initialState):
+	for rule in rules:
+		print "---------------"
+		print rule
 	createdRules = []
 	atomicSignatures, structureSignatures, atomicNames = obtainSignatures(rules, initialState)
+	print atomicSignatures
+	print structureSignatures
 	for rule in rules:
 		# removal of stoichiometry goes here
 		lhs = rule['children'][0]['children'][0]['children']
 		rhs = rule['children'][0]['children'][1]['children']
 		I = len(lhs) - 1
-		chi = createComplexes(lhs + rhs, atomicNames)
+		chi, atomicSignatures, structureSignatures = \
+			createComplexes(lhs + rhs, atomicNames, atomicSignatures, structureSignatures)
 		sequences = map(lambda complex: complex.sequence, chi)
 		omega = sum(sequences, [])
 		indexMap = getIndexmap(sequences)
 		indices = getIndices(indexMap[I], len(omega) - 1)
 		createdRules.append(Rule(chi, omega, I, indexMap, indices))
 	initialState = createComplexes(map(lambda init: \
-		init['children'][0]['children'][0]['children'][0], initialState), atomicNames)
+		init['children'][0]['children'][0]['children'][0], initialState), atomicNames, atomicSignatures, structureSignatures)
 
 	return createdRules, atomicSignatures, structureSignatures, initialState
 
-def createComplexes(complexes, atomicNames):
+def createComplexes(complexes, atomicNames, atomicSignatures, structureSignatures):
 	createdComplexes = []
 	for complex in complexes:
 		sequence = []
 		compartment = complex['children'][0]['children'][-1]['children'][0]['entity']['token']
 		if len(complex['children'][0]['children']) > 2:
-			return # removal of nested complexes goes here
+			composition = []
+			for comp in complex['children'][0]['children'][:-1]:
+				print comp
+				composition.append(Complex(createAgents(comp['children'], atomicNames), compartment))
+			print "----------------------------------------"
+			agent = composition[0]
+			for comp in composition[1:]:
+				agent = mergeComplexes(agent, comp)
+			atomicSignatures, structureSignatures = updateSignatures(agent, atomicSignatures, structureSignatures)
+			createdComplexes.append(agent)
 		else:
 			sequence = complex['children'][0]['children'][0]['children']
-		agents = createAgents(sequence, atomicNames)
-		for _ in range(int(complex['token'])): # this removes stoichiometry from init, not rules !
-			createdComplexes.append(Complex(agents, compartment))
-	return createdComplexes
+			agents = createAgents(sequence, atomicNames)
+			for _ in range(int(complex['token'])): # this removes stoichiometry from init, not rules !
+				createdComplexes.append(Complex(agents, compartment))
+	for complex in createdComplexes:
+		print complex
+	return createdComplexes, atomicSignatures, structureSignatures
+
+def mergeComplexes(complex_left, complex_right):
+	if len(complex_left.sequence) == 1 and len(complex_right.sequence) == 1:
+		if complex_left.sequence[0].name not in complex_right.sequence[0].getAtomicNames():
+			atom = complex_left.sequence[0]
+			struct = complex_right.sequence[0]
+			return Complex([StructureAgent(struct.name, struct.composition | {atom})], complex_right.compartment)
+		else:
+			return complex_right
+	else:
+		return
+
+def updateSignatures(complex, atomicSignatures, structureSignatures):
+	for agent in complex.sequence:
+		if isinstance(agent, AtomicAgent):
+			atomicSignatures[agent.name] |= {agent.state}
+		else:
+			structureSignatures[agent.name] |= agent.getAtomicNames()
+	return atomicSignatures, structureSignatures
 
 def createAgents(sequence, atomicNames):
 	createdAgents = []
